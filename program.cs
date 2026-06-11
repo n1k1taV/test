f2daa630c8a2727781db9d0eb1a68195be312fe4
n1k1taV <nik.voronin63@mail.ru>
2026-06-11 14:23:07 +0300

    добавил файлы в репозиторий

--- program.cs
+++ program.cs
@@ -1,2 +1,18 @@
-int a = 6;
-Console.WriteLine(a);
+﻿namespace Git_test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine(a);
+            int b = 7;
+            Console.WriteLine(b);
+
+            int c = 8;
+            Console.WriteLine(c);
+
+            int d = 9; 
+        }
+    }
+}
